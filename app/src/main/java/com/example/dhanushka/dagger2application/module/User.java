b2ea1ab89a732cce85bbe3a5266637a05e680a72
Dhanushka Basnayaka <dhanushkab.emedia@gmail.com>
2017-10-10 21:17:16 +0530

    update

---
+++ app/src/main/java/com/example/dhanushka/dagger2application/module/User.java
@@ -0,0 +1,8 @@
+package com.example.dhanushka.dagger2application.module;
+
+/**
+ * Created by dhanushka on 10/10/2017.
+ */
+
+public class User {
+}
--- app/src/main/java/com/example/dhanushka/dagger2application/network/ApiService.java
+++ app/src/main/java/com/example/dhanushka/dagger2application/network/ApiService.java
@@ -1,8 +1,25 @@
 package com.example.dhanushka.dagger2application.network;
 
+import com.example.dhanushka.dagger2application.module.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by dhanushka on 10/10/2017.
  */
 
 public interface ApiService {
+
+    @GET("users/{username}/repos")
+    Call<List<User>> getReposForUser(@Path("username") String username);
+
+    @GET("repositories")
+    Call<List<User>> getAllRepos();
+
+    @GET("users/{username}")
+    Call<User> getUser(@Path("username") String username);
 }
